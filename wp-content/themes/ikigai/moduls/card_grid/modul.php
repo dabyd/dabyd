7aac9b9ef5ea0ff816da9bfa78342054cde8f77f
David Herrero <david@gold.barcelona>
2026-01-08 08:33:09 +0100

    Subida con formulario

--- wp-content/themes/ikigai/moduls/card_grid/modul.php
+++ wp-content/themes/ikigai/moduls/card_grid/modul.php
@@ -2,12 +2,15 @@
 	$total = ikg_get_acf_value('caixes');
 ?>
 	<!-- card grid -->
-	<section class="container card-grid <?php ikg_value('posicio_caixes') ?> <?php ikg_get_variant(); ?>">
-		<?php for( $n = 0; $n < $total; $n++ ) : ?>
-			<?php ikg_setbase( 'caixes_' . $n . '_' ); ?>
-			<div class="card">
-				<h3><?php ikg_value('titol'); ?></h3>
-				<p class="section-subtitle"><?php echo ikg_get_text('texte'); ?></p>
-			</div>
-		<?php endfor; ?>
+	<section class="card-grid <?php ikg_value('posicio_caixes') ?> <?php ikg_get_variant(); ?>">
+		<h2 class="section-title"><?php ikg_value('titol'); ?></h2>
+		<div class="container">
+			<?php for( $n = 0; $n < $total; $n++ ) : ?>
+				<?php ikg_setbase( 'caixes_' . $n . '_' ); ?>
+				<div class="card">
+					<h3><?php ikg_value('titol'); ?></h3>
+					<p class="section-subtitle"><?php echo ikg_get_text('texte'); ?></p>
+				</div>
+			<?php endfor; ?>
+		</div>
 	</section>
