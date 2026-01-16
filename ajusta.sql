a23447ce00ad1720e076bceabd98babbc5754bcc
David Herrero <david@gold.barcelona>
2026-01-16 17:35:21 +0100

    Arreglo varias cosas

--- ajusta.sql
+++
@@ -1,3 +0,0 @@
-UPDATE `dabyd`.`wp_options` SET `option_value` = 'https://dabyd.com' WHERE (`option_id` = '1');
-UPDATE `dabyd`.`wp_options` SET `option_value` = 'https://dabyd.com' WHERE (`option_id` = '2');
-
